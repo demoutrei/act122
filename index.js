e80c266f053c9f5182f7ab572fcc16ac652e7b13
demoutrei <works.demoutrei@gmail.com>
2026-05-02 19:33:11 +0800

    [UPDATED] Log in mechanism

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
+const inputLogInAccountId = document.querySelector("#logIn-info-accountId");
+const inputLogInPassword = document.querySelector("#logIn-info-password");
 const logInButton = document.querySelector("#button-logIn");
 const logInTypesArray = document.querySelectorAll(".logIn-type");
+
+
+inputLogInAccountId.addEventListener(
+  "input",
+  (_) => {
+    if (!inputLogInAccountId.value || !inputLogInPassword.value) {
+      logInButton.setAttribute("disabled", "disabled");
+    } else {
+      logInButton.removeAttribute("disabled");
+    }
+  }
+)
+
+inputLogInPassword.addEventListener(
+  "input",
+  (_) => {
+    if (!inputLogInAccountId.value || !inputLogInPassword.value) {
+      logInButton.setAttribute("disabled", "disabled");
+    } else {
+      logInButton.removeAttribute("disabled");
+    }
+  }
+)
 
 
 logInTypesArray.forEach(
