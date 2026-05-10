42d5104e29aaf08ec689720e23398ca21fb2c08b
demoutrei <works.demoutrei@gmail.com>
2026-05-10 20:46:46 +0800

    [ADDED] /student/profile: toast notifications

--- student/profile/index.js
+++ student/profile/index.js
@@ -1,3 +1,29 @@
+function notify(title, message) {
+  const container = document.querySelector("#container-notifications");
+
+  const toast = document.createElement("div");
+  toast.classList.add("notification");
+  const toastTitle = document.createElement("div");
+  toastTitle.classList.add("notification-title");
+  toastTitle.innerText = title;
+  toast.appendChild(toastTitle);
+  const toastMessage = document.createElement("div");
+  toastMessage.classList.add("notification-message");
+  toastMessage.innerText = message;
+  toast.appendChild(toastMessage);
+  container.prepend(toast);
+  toast.onclick = () => remove(toast);
+  setTimeout(() => remove(toast), 4000);
+
+  function remove(t) {
+    toast.classList.add("fadeOut");
+    toast.addEventListener(
+      "transitionend",
+      () => toast.remove()
+    )
+  }
+}
+
 function scrollToView(elementId) {
   document.querySelector(elementId).scrollIntoView({
     behavior: "smooth",
@@ -9,64 +35,33 @@ document.querySelector("#section-notifications-email").addEventListener(
   "click",
   (_) => {
     document.querySelector("#section-notifications-email").classList.toggle("active");
+    if (document.querySelector("#section-notifications-email").classList.contains("active")) {
+      title = "Enabled 'Email Address' notifications";
+      message = "You will start receiving notifications through your email address.";
+    } else {
+      title = "Disabled 'Email Address' notifications";
+      message = "You will now stop receiving notifications from your email address."
+    }
+    notify(title, message);
   }
 )
 
 document.querySelector("#section-notifications-contactNumber").addEventListener(
   "click",
-  (_) => document.querySelector("#section-notifications-contactNumber").classList.toggle("active")
+  (_) => {
+    document.querySelector("#section-notifications-contactNumber").classList.toggle("active");
+    if (document.querySelector("#section-notifications-contactNumber").classList.contains("active")) {
+      title = "Enabled 'Contact Number' notifications";
+      message = "You will start receiving notifications through your contact number.";
+    } else {
+      title = "Disabled 'Contact Number' notifications";
+      message = "You will now stop receiving notifications from your contact number."
+    }
+    notify(title, message);
+  }
 )
 
-(new IntersectionObserver(
-  (entries) => {
-    entries.forEach(
-      (entry) => {
-        const target = document.querySelector("#navigation-account");
-        if (entry.isIntersecting) {
-          target.classList.add("active");
-        } else {
-          target.classList.remove("active");
-        }
-      }
-    )
-  },
-  {
-    threshold: 0.5
-  }
-)).observe(document.querySelector("#section-account"));
-
-(new IntersectionObserver(
-  (entries) => {
-    entries.forEach(
-      (entry) => {
-        const target = document.querySelector("#navigation-notifications");
-        if (entry.isIntersecting) {
-          target.classList.add("active");
-        } else {
-          target.classList.remove("active");
-        }
-      }
-    )
-  },
-  {
-    threshold: 0.5
-  }
-)).observe(document.querySelector("#section-notifications"));
-
-(new IntersectionObserver(
-  (entries) => {
-    entries.forEach(
-      (entry) => {
-        const target = document.querySelector("#navigation-logOut");
-        if (entry.isIntersecting) {
-          target.classList.add("active");
-        } else {
-          target.classList.remove("active");
-        }
-      }
-    )
-  },
-  {
-    threshold: 0.5
-  }
-)).observe(document.querySelector("#section-logOut"));
+document.querySelector("#section-account-studentId").addEventListener(
+  "click",
+  (_) => notify("", "Copied your Student ID to clipboard")
+)
